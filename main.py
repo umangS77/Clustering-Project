2730e1b9d0bb42e01add52d6b2ff0327ffa74101
umangS77 <umang.srivastava.777@gmail.com>
2021-10-06 21:55:45 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import pickle
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import MinMaxScaler,StandardScaler
+import matplotlib.pyplot as plt
+import seaborn as sns
+
